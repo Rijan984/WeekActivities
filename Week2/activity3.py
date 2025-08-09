da859403415808e4060be2a4110841f1c86446f8
Rijan984 <57378576+Rijan984@users.noreply.github.com>
2025-08-10 05:14:31 +0545

    update in code

--- Week2/activity3.py
+++ Week2/activity3.py
@@ -1,17 +1,24 @@
 class StringManipulator:
     def __init__(self, text):
         self.text = text
-        
 
     def find_character(self, char):
         return self.text.find(char)
+    
+    def lenFind(self):
+        return len(self.text)
+    
+    def upper_case(self):
+        return self.text.upper()
     
 
 name = StringManipulator("example")
 
 
 result = name.find_character('x')
-print("My lenght is:", len(name.text))
-print("UpperCase:", name.text.upper() )
+print("My lenght is:", name.lenFind())
+print("UpperCase:", name.upper_case() )
+
+# print("My lenght is:", len(name.text))
+# print("UpperCase:", name.text.upper() )
 print(result)
-# print("length is:", lenName)
